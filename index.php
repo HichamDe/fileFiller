8d5609cb43c8f29b02fceed77fe17262a7e3d97f
HichamDe <hicham.iamiri200@gmail.com>
2023-05-09 22:39:26 +0100

    df

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 require_once("module/line.php");
-//* php index.php "Id:0,Complex:Text:none:Example+Id:1+Text:none:@gmail.com" 15 "," data.mail.com" 15 "," data.csv
 
-$code = $argv[1];
-$repeat = $argv[2];
-$seperator = $argv[3];
-$file = $argv[4];
+$configs = parse_ini_file("config.ini");
 
-for($lineId=0 ; $lineId < $repeat; $lineId++){
+for($lineId=0 ; $lineId < $configs["repeat"]; $lineId++){
 
-    $line = new Line($code,$seperator,$lineId);
-    $line->pushTo($file);
+    $line = new Line($configs["code"],$configs["seperator"],$lineId);
+    $line->pushTo($configs["file"]);
 
 } 
